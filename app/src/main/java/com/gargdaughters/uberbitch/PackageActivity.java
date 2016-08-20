62e3798b5059d61b231d88318cc1f7bf57e524c9
shruti garg <gshruti.90@gmail.com>
2016-08-20 18:15:22 +0530

    committing the new dropdown changes

---
+++ app/src/main/java/com/gargdaughters/uberbitch/PackageActivity.java
@@ -0,0 +1,13 @@
+package com.gargdaughters.uberbitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PackageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.packages_add);
+    }
+}
