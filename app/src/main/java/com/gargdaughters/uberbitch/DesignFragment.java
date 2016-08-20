991ad1f9b60e63fffbfbb49b529398965900b1e7
shikhagarg <gargshikha02@gmail.com>
2016-08-20 18:04:20 +0530

    Add fragments for corresponding tab

---
+++ app/src/main/java/com/gargdaughters/uberbitch/DesignFragment.java
@@ -0,0 +1,27 @@
+package com.gargdaughters.uberbitch;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by derll on 8/20/2016.
+ */
+
+public class DesignFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_design,container,false);
+    }
+}
